4af0f810d8d5d0bb9eaf7a3b6f65dbb1bcecb79c
olivier.carpentier@gmail.com <ocarpen@microsoft.com>
2016-10-02 19:56:33 +0200

    Doc

--- README.md
+++ README.md
@@ -1,26 +1,68 @@
-## sp-client-custom-fields
+# SPFx Custom Fields Library
 
-This is where you include your web part docs.
+> Note: The SharePoint Framework is currently in preview and is subject to change. SharePoint Framework client-side web parts are not currently supported for use in production enviornments.
 
-### Building the code
+This example is a kit of **19+ components** to customize SPFx web parts custom fields, to make the
+**optimal experience to edit your Web parts**.
+
+These samples show how to implement custom fields with the new SharePoint Framework (SPFx). These samples
+include an implementation of **PropertyFieldDatePicker**, **PropertyFieldDateTimePicker**, **PropertyFieldPicturePicker**, **PropertyFieldDocumentPicker**,
+**PropertyFieldPeoplePicker**, **PropertyFieldColorPicker**,
+**PropertyFieldFontPicker**, **PropertyFieldFontSizePicker**, **PropertyFieldIconPicker**, **PropertyFieldDisplayMode**,
+**PropertyFieldCustomList**,
+ **PropertyFieldSPListPicker**,  **PropertyFieldSPListQuery**,**PropertyFieldSPListMultiplePicker**, **PropertyFieldSPFolderPicker**,
+ **PropertyFieldPhoneNumber**, **PropertyFieldMaskedInput**, **PropertyFieldMapPicker**
+ controls, based on the Office UI Fabric framework and React.
+
+# Overview
+
+![Overview](./assets/Overview.gif)
+
+# Custom fields documentation
+
+You can use these custom fields to your own client side web parts. You can find bellow the list of
+all these custom fields and you can click on the custom field name to consult the documentation.
+
+Custom Field | Description |  Overview
+------------ | ----------- | -----------
+[PropertyFieldDatePicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDatePicker) | Custom field to select a Date with a mini-calendar based on the Office UI Fabric DatePicker control | ![PropertyFieldDatePicker](./assets/OverviewDatePicker.png)
+[PropertyFieldDateTimePicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDateTimePicker) | Custom field to select a Date Time with a mini-calendar based on the Office UI Fabric DatePicker control | ![PropertyFieldDateTimePicker](./assets/OverviewDateTimePicker.png)
+[PropertyFieldColorPicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldColorPicker) | Custom field to select a Color with a visual RGB editor based on the Office UI Fabric ColorPicker control | ![PropertyFieldColorPicker](./assets/OverviewColorPicker.png)
+[PropertyFieldFontPicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldFontPicker) | Custom field to select a Font with a visual dropdown box and fonts preview | ![PropertyFieldFontPicker](./assets/OverviewFontPicker.png)
+[PropertyFieldFontSizePicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldFontSizePicker) | Custom field to select a Font size with size preview as drop down | ![PropertyFieldFontSizePicker](./assets/OverviewFontSizePicker.png)
+[PropertyFieldIconPicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldIconPicker) | Custom field to select an Icon style in the Office UI Fabric icons list | ![PropertyFieldIconPicker](./assets/OverviewIconPicker.png)
+[PropertyFieldDisplayMode](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDisplayMode) | Custom field to select a list display mode (list or tiles) | ![PropertyFieldDisplayMode](./assets/OverviewDisplayMode.png)
+[PropertyFieldPeoplePicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPeoplePicker) | Custom field to select users from the SharePoint users database with a search field. Start to enter a lastname or a firstname, and pick a user  | ![PropertyFieldPeoplePicker](./assets/OverviewPeoplePicker.png)
+[PropertyFieldPicturePicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPicturePicker)| Custom field to select a picture thanks to a SharePoint visual browser.  | ![PropertyFieldPicturePicker](./assets/OverviewPicturePicker.png)
+[PropertyFieldDocumentPicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDocumentPicker)| Custom field to select a document (docx, pptx, pdf, etc.) thanks to a SharePoint visual browser.  | ![PropertyFieldDocumentPicker](./assets/OverviewDocumentPicker.png)
+[PropertyFieldCustomList](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldCustomList)| Custom field to select a collection of data with forms as an object property of your web part  | ![PropertyFieldCustomList](./assets/OverviewCustomList.png)
+[PropertyFieldSPListPicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPListPicker)| Custom field to select a list from the current SharePoint web site.   | ![PropertyFieldSPListPicker](./assets/OverviewSPListPicker.png)
+[PropertyFieldSPListMultiplePicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPListMultiplePicker)| Custom field to select multiple lists from the current SharePoint web site.   | ![PropertyFieldSPListMultiplePicker](./assets/OverviewSPListMultiplePicker.png)
+[PropertyFieldSPListQuery](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPListQuery)| Custom field to query parameters and to get a REST web url to get items.   | ![PropertyFieldSPListQuery](./assets/OverviewSPListQuery.png)
+[PropertyFieldSPFolderPicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPFolderPicker)| Custom field to select a folder from the current SharePoint web site.   | ![PropertyFieldSPFolderPicker](./assets/OverviewSPFolderPicker.png)
+[PropertyFieldPassword](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPassword) | Custom field to select a password | ![PropertyFieldPassword](./assets/OverviewPassword.png)
+[PropertyFieldPhoneNumber](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPhoneNumber) | Custom field to select a phone number with a masked control based on phone numbers international formats. | ![PropertyFieldPhoneNumber](./assets/OverviewPhoneNumber.png)
+[PropertyFieldMaskedInput](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldMaskedInput) | Custom field to add a text input with a specified masked based on regexp. | ![PropertyFieldMaskedInput](./assets/OverviewMaskedInput.png)
+[PropertyFieldMapPicker](https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldMapPicker) | Custom field to add a gps localisation with map preview. | ![PropertyFieldMapPicker](./assets/OverviewMapPicker.png)
+
+# Build and run this sample in the SharePoint workbench
 
 ```bash
 git clone the repo
 npm i
-npm i -g gulp
-gulp
+tsd install
+gulp serve
 ```
 
-This package produces the following:
+If you need more information about to develop SharePoint Framework client side web part, deploy and test it on your workbench
+station, you can consult the following tutorial: https://github.com/SharePoint/sp-dev-docs/wiki/Setup-SharePoint-Tenant
 
-* lib/* commonjs components - this allows this package to be reused from other packages.
-* dist/* - a single bundle containing the components used for uploading to a cdn pointing a registered Sharepoint webpart library to.
-* example/* a test page that hosts all components in this package.
+# The MIT License (MIT)
 
-### Build options
+Copyright (c) 2016 Olivier Carpentier
 
-gulp nuke - TODO
-gulp test - TODO
-gulp watch - TODO
-gulp build - TODO
-gulp deploy - TODO
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- src/webparts/test/components/Test.tsx
+++ src/webparts/test/components/Test.tsx
@@ -43,27 +43,27 @@ export default class Test extends React.Component<ITestProps, {}> {
               <p>
                 <i className="ms-Icon ms-Icon--Font" aria-hidden="true"></i>&nbsp;
                 <b>Font</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldFontPicker" target="_doc">(Doc)</a> : &nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldFontPicker" target="_doc">(Doc)</a> : &nbsp;
                 {this.props.font}
 
               </p>
               <p>
                 <i className="ms-Icon ms-Icon--FontSize" aria-hidden="true"></i>&nbsp;
                 <b>Font Size</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldFontSizePicker" target="_doc">(Doc)</a> : &nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldFontSizePicker" target="_doc">(Doc)</a> : &nbsp;
                 {this.props.fontSize}
 
               </p>
               <p >
                 <i className="ms-Icon ms-Icon--FontColor" aria-hidden="true"></i>&nbsp;
                 <b>Color</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldColorPicker" target="_doc">(Doc)</a> : &nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldColorPicker" target="_doc">(Doc)</a> : &nbsp;
                 {this.props.color}
               </p>
               <p >
                 <i className="ms-Icon ms-Icon--Waffle" aria-hidden="true"></i>&nbsp;
                 <b>Icon</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldIconPicker" target="_doc">(Doc)</a> : &nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldIconPicker" target="_doc">(Doc)</a> : &nbsp;
                 <i className={'ms-Icon ' + this.props.icon} aria-hidden="true" style={{fontSize:'large'}}></i>
                 &nbsp;{this.props.icon}
               </p>
@@ -75,50 +75,50 @@ export default class Test extends React.Component<ITestProps, {}> {
               <p >
                 <i className="ms-Icon ms-Icon--List" aria-hidden="true"></i>&nbsp;
                 <b>Custom List</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldCustomList" target="_doc">(Doc)</a> : &nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldCustomList" target="_doc">(Doc)</a> : &nbsp;
                 {JSON.stringify(this.props.customList)}
               </p>
               <p >
                 <i className="ms-Icon ms-Icon--Lock" aria-hidden="true"></i>&nbsp;
                 <b>Password</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldPassword" target="_doc">(Doc)</a> : &nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPassword" target="_doc">(Doc)</a> : &nbsp;
                 {this.props.password}
               </p>
               <p>
                 <i className="ms-Icon ms-Icon--CalendarWorkWeek" aria-hidden="true"></i>&nbsp;
                 <b>Date</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldDatePicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDatePicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.date}
               </p>
               <p >
                 <i className="ms-Icon ms-Icon--CalendarWorkWeek" aria-hidden="true"></i>&nbsp;
                 <b>Date ISO</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldDatePicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDatePicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.date2}
               </p>
               <p >
                 <i className="ms-Icon ms-Icon--CalendarWorkWeek" aria-hidden="true"></i>&nbsp;
                 <b>Date Time</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldDateTimePicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDateTimePicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.datetime}
               </p>
 
               <p>
                 <i className="ms-Icon ms-Icon--Phone" aria-hidden="true"></i>&nbsp;
                 <b>Phone Number</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldPhoneNumber" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPhoneNumber" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.phone}
               </p>
               <p>
                 <i className="ms-Icon ms-Icon--PaymentCard" aria-hidden="true"></i>&nbsp;
                 <b>Credit Card</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldMaskedInput" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldMaskedInput" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.maskedInput}
               </p>
               <p>
                 <i className="ms-Icon ms-Icon--MapPin" aria-hidden="true"></i>&nbsp;
                 <b>Geolocation</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldMapPicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldMapPicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.geolocation}
               </p>
 
@@ -129,7 +129,7 @@ export default class Test extends React.Component<ITestProps, {}> {
               <div>
                 <i className="ms-Icon ms-Icon--PeopleAdd" aria-hidden="true"></i>&nbsp;
                 <b>Users</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldPeoplePicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPeoplePicker" target="_doc">(Doc)</a> :&nbsp;
 
                 <ul>
                 {
@@ -149,43 +149,43 @@ export default class Test extends React.Component<ITestProps, {}> {
               <p>
                 <i className="ms-Icon ms-Icon--Picture" aria-hidden="true"></i>&nbsp;
                 <b>Picture</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldPicturePicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldPicturePicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.picture}
               </p>
               <p>
                 <i className="ms-Icon ms-Icon--Document" aria-hidden="true"></i>&nbsp;
                 <b>Document</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldDocumentPicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDocumentPicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.document}
               </p>
               <p>
                 <i className="ms-Icon ms-Icon--List" aria-hidden="true"></i>&nbsp;
                 <b>List</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldSPListPicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPListPicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.list}
               </p>
               <p>
                 <i className="ms-Icon ms-Icon--List" aria-hidden="true"></i>&nbsp;
                 <b>Query</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldSPListQuery" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPListQuery" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.query}
               </p>
               <p >
                 <i className="ms-Icon ms-Icon--List" aria-hidden="true"></i>&nbsp;
                 <b>Display Mode</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldDisplayMode" target="_doc">(Doc)</a> : &nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldDisplayMode" target="_doc">(Doc)</a> : &nbsp;
                 {this.props.displayMode}
               </p>
               <p >
                 <i className="ms-Icon ms-Icon--FolderSearch" aria-hidden="true"></i>&nbsp;
                 <b>Folder</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldSPFolderPicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPFolderPicker" target="_doc">(Doc)</a> :&nbsp;
                 {this.props.folder}
               </p>
               <div >
                 <i className="ms-Icon ms-Icon--List" aria-hidden="true"></i>&nbsp;
                 <b>Lists</b>
-                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/spfx-custom-fields/wiki/PropertyFieldSPListMultiplePicker" target="_doc">(Doc)</a> :&nbsp;
+                &nbsp;<a className="ms-fontSize-sPlus" href="https://github.com/OlivierCC/sp-client-custom-fields/wiki/PropertyFieldSPListMultiplePicker" target="_doc">(Doc)</a> :&nbsp;
                 <ul>
                 {
                   lists.map((element: string, i:number) => {
