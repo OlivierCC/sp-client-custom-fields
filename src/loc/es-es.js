d6b3e895d4c6db2b972143fcc7ec2dda9c52134f
olivier.carpentier@gmail.com <ocarpen@microsoft.com>
2016-10-06 18:28:26 +0200

    Add translations

---
+++ src/loc/es-es.js
@@ -0,0 +1,109 @@
+define([], function() {
+  return {
+    //Custom List labels
+    //Copy the following labels in your project if you want to use the SPListQuery
+    "CustomListAddItem": "Añadir",
+    "CustomListBack": "Volver",
+    "CustomListTrue": "Verdadera",
+    "CustomListFalse": "Falso",
+    "CustomListOK": "Vale",
+    "CustomListCancel": "Cancelar",
+    "CustomListEdit": "Editar",
+    "CustomListDel": "Eliminar",
+    "CustomListYes": "Sí",
+    "CustomListNo": "No",
+    "CustomListConfirmDel": "Confirmación",
+    "CustomListConformDelMssg": "¿Está seguro que desea eliminar este elemento?",
+    "CustomListFieldMissing": "Error '{0}' campo es obligatorio",
+
+    //SPListQuery labels
+    //Copy the following labels in your project if you want to use the SPListQuery
+    "SPListQueryList": "Lista",
+    "SPListQueryOrderBy": "Ordenar por",
+    "SPListQueryArranged": "Orden",
+    "SPListQueryMax": "Máximo",
+    "SPListQueryAdd": "Añadir un filtro",
+    "SPListQueryRemove": "Eliminar",
+    "SPListQueryOperatorEq": "Igual",
+    "SPListQueryOperatorNe": "A diferencia de",
+    "SPListQueryOperatorStartsWith": "Comienza con",
+    "SPListQueryOperatorSubstringof": "Parte de bajo",
+    "SPListQueryOperatorLt":  "Menos de",
+    "SPListQueryOperatorLe": "Menor o igual",
+    "SPListQueryOperatorGt": "Más de",
+    "SPListQueryOperatorGe": "Mayor o igual",
+
+    //PicturePicker labels
+    //Copy the following labels in your project if you want to use the PicturePicker
+    "PicturePickerTitle": "Seleccione una imagen",
+    "PicturePickerRecent": "Recientes",
+    "PicturePickerSite": "Sitio",
+    "PicturePickerButtonSelect": "Seleccione una imagen",
+    "PicturePickerButtonReset": "Borrar",
+
+    //DocumentPicker labels
+    //Copy the following labels in your project if you want to use the DocumentPicker
+    "DocumentPickerTitle": "Seleccione un documento de",
+    "DocumentPickerRecent": "Recientes",
+    "DocumentPickerSite": "Sitio",
+    "DocumentPickerButtonSelect": "Seleccione un documento de",
+    "DocumentPickerButtonReset": "Borrar",
+
+    //PeoplePicker labels
+    //Copy the following labels in your project if you want to use the PeoplePicker
+    "PeoplePickerSuggestedContacts": "Contactos sugeridos",
+    "PeoplePickerNoResults": "No hay resultados",
+    "PeoplePickerLoading": "Carga...",
+
+    //SPListPicker labels
+    //Copy the following labels in your project if you want to use the SPListPicker
+    "SPListPickerLoading": "Carga...",
+
+    //SPFolderPicker labels
+    //Copy the following labels in your project if you want to use the SPFolderPicker
+    "SPFolderPickerDialogTitle": "Seleccione un directorio",
+    "SPFolderPickerSelectButton": "Seleccione",
+    "SPFolderPickerCancelButton": "Cancelar",
+
+    //DatePicker labels
+    //Copy the following labels in your project if you want to use the DatePicker
+    "DatePickerMonthLongJanuary": "Enero",
+    "DatePickerMonthShortJanuary": "Ene",
+    "DatePickerMonthLongFebruary": "Febrero",
+    "DatePickerMonthShortFebruary": "Feb",
+    "DatePickerMonthLongMarch": "Marzo",
+    "DatePickerMonthShortMarch": "Mar",
+    "DatePickerMonthLongApril": "Abril",
+    "DatePickerMonthShortApril": "Abr",
+    "DatePickerMonthLongMay": "Mayo",
+    "DatePickerMonthShortMay": "May",
+    "DatePickerMonthLongJune": "Junio",
+    "DatePickerMonthShortJune": "Jun",
+    "DatePickerMonthLongJuly": "Julio",
+    "DatePickerMonthShortJuly": "Jul",
+    "DatePickerMonthLongAugust": "Agosto",
+    "DatePickerMonthShortAugust": "Ago",
+    "DatePickerMonthLongSeptember": "Septiembre",
+    "DatePickerMonthShortSeptember": "Sep",
+    "DatePickerMonthLongOctober": "Octubre",
+    "DatePickerMonthShortOctober": "Oct",
+    "DatePickerMonthLongNovember": "Noviembre",
+    "DatePickerMonthShortNovember": "Nov",
+    "DatePickerMonthLongDecember": "Diciembre",
+    "DatePickerMonthShortDecember": "Dic",
+    "DatePickerDayLongSunday": "Domingo",
+    "DatePickerDayShortSunday": "Dom",
+    "DatePickerDayLongMonday": "Lunes",
+    "DatePickerDayShortMonday": "Lun",
+    "DatePickerDayLongTuesday": "Martes",
+    "DatePickerDayShortTuesday": "Mar",
+    "DatePickerDayLongWednesday": "Miércoles",
+    "DatePickerDayShortWednesday": "Mié",
+    "DatePickerDayLongThursday": "Jueves",
+    "DatePickerDayShortThursday": "Jue",
+    "DatePickerDayLongFriday": "Viernes",
+    "DatePickerDayShortFriday": "Vie",
+    "DatePickerDayLongSaturday": "Sábado",
+    "DatePickerDayShortSaturday": "Sab"
+  }
+});
---
+++ src/loc/it-it.js
@@ -0,0 +1,109 @@
+define([], function() {
+  return {
+    //Custom List labels
+    //Copy the following labels in your project if you want to use the SPListQuery
+    "CustomListAddItem": "Aggiungere",
+    "CustomListBack": "Ritorno",
+    "CustomListTrue": "Vero",
+    "CustomListFalse": "Falso",
+    "CustomListOK": "OK",
+    "CustomListCancel": "Annulla",
+    "CustomListEdit": "Modifica",
+    "CustomListDel": "Elimina",
+    "CustomListYes": "Sì",
+    "CustomListNo": "No",
+    "CustomListConfirmDel": "Conferma",
+    "CustomListConformDelMssg": "Sei sicuro di che voler eliminare questo elemento?",
+    "CustomListFieldMissing": "Errore '{0}' campo è obbligatorio",
+
+    //SPListQuery labels
+    //Copy the following labels in your project if you want to use the SPListQuery
+    "SPListQueryList": "Elenco",
+    "SPListQueryOrderBy": "Ordina per",
+    "SPListQueryArranged": "Ordine",
+    "SPListQueryMax": "Massimo",
+    "SPListQueryAdd": "Aggiungere un filtro",
+    "SPListQueryRemove": "Elimina",
+    "SPListQueryOperatorEq": "Pari",
+    "SPListQueryOperatorNe": "Diverso da",
+    "SPListQueryOperatorStartsWith": "Inizia con",
+    "SPListQueryOperatorSubstringof": "Parte di sotto",
+    "SPListQueryOperatorLt":  "Meno di",
+    "SPListQueryOperatorLe": "Minore o uguale",
+    "SPListQueryOperatorGt": "Più di",
+    "SPListQueryOperatorGe": "Maggiore o uguale",
+
+    //PicturePicker labels
+    //Copy the following labels in your project if you want to use the PicturePicker
+    "PicturePickerTitle": "Selezionare un'immagine",
+    "PicturePickerRecent": "Recenti",
+    "PicturePickerSite": "Sito",
+    "PicturePickerButtonSelect": "Selezionare un'immagine",
+    "PicturePickerButtonReset": "Cancellare",
+
+    //DocumentPicker labels
+    //Copy the following labels in your project if you want to use the DocumentPicker
+    "DocumentPickerTitle": "Selezionare un documento",
+    "DocumentPickerRecent": "Recenti",
+    "DocumentPickerSite": "Sito",
+    "DocumentPickerButtonSelect": "Selezionare un documento",
+    "DocumentPickerButtonReset": "Cancellare",
+
+    //PeoplePicker labels
+    //Copy the following labels in your project if you want to use the PeoplePicker
+    "PeoplePickerSuggestedContacts": "Contatti suggeriti",
+    "PeoplePickerNoResults": "Nessun risultato",
+    "PeoplePickerLoading": "Caricamento...",
+
+    //SPListPicker labels
+    //Copy the following labels in your project if you want to use the SPListPicker
+    "SPListPickerLoading": "Caricamento...",
+
+    //SPFolderPicker labels
+    //Copy the following labels in your project if you want to use the SPFolderPicker
+    "SPFolderPickerDialogTitle": "Selezionare una directory",
+    "SPFolderPickerSelectButton": "Selezionare",
+    "SPFolderPickerCancelButton": "Annulla",
+
+    //DatePicker labels
+    //Copy the following labels in your project if you want to use the DatePicker
+    "DatePickerMonthLongJanuary": "Gennaio",
+    "DatePickerMonthShortJanuary": "Gen",
+    "DatePickerMonthLongFebruary": "Febbraio",
+    "DatePickerMonthShortFebruary": "Feb",
+    "DatePickerMonthLongMarch": "Marzo",
+    "DatePickerMonthShortMarch": "Mar",
+    "DatePickerMonthLongApril": "Aprile",
+    "DatePickerMonthShortApril": "Apr",
+    "DatePickerMonthLongMay": "Maggio",
+    "DatePickerMonthShortMay": "Mag",
+    "DatePickerMonthLongJune": "Giugno",
+    "DatePickerMonthShortJune": "Giu",
+    "DatePickerMonthLongJuly": "Luglio",
+    "DatePickerMonthShortJuly": "Lug",
+    "DatePickerMonthLongAugust": "Agosto",
+    "DatePickerMonthShortAugust": "Ago",
+    "DatePickerMonthLongSeptember": "Settembre",
+    "DatePickerMonthShortSeptember": "Set",
+    "DatePickerMonthLongOctober": "Ottobre",
+    "DatePickerMonthShortOctober": "Ott",
+    "DatePickerMonthLongNovember": "Novembre",
+    "DatePickerMonthShortNovember": "Nov",
+    "DatePickerMonthLongDecember": "Dicembre",
+    "DatePickerMonthShortDecember": "Dic",
+    "DatePickerDayLongSunday": "Domenica",
+    "DatePickerDayShortSunday": "Dom",
+    "DatePickerDayLongMonday": "Lunedì",
+    "DatePickerDayShortMonday": "Lun",
+    "DatePickerDayLongTuesday": "Martedì",
+    "DatePickerDayShortTuesday": "Mar",
+    "DatePickerDayLongWednesday": "Mercoledì",
+    "DatePickerDayShortWednesday": "Mer",
+    "DatePickerDayLongThursday": "Giovedì",
+    "DatePickerDayShortThursday": "Gio",
+    "DatePickerDayLongFriday": "Venerdì",
+    "DatePickerDayShortFriday": "Ven",
+    "DatePickerDayLongSaturday": "Sabato",
+    "DatePickerDayShortSaturday": "Sab"
+  }
+});
